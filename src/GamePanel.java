e3dd2996e30606182ff7b68478eb07eb67415e9d
Arcxnum <gamingfardsletsgo@gmail.com>
2024-12-09 15:26:20 -0500

    Added the game loop

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -25,7 +25,24 @@ public class GamePanel extends JPanel implements Runnable {
 	@Override
 	public void run() {
 		
+		// GAME LOOP
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
 		
+		while (gameThread != null) {
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime)/drawInterval;
+			lastTime = currentTime;
+			
+			if (delta >= 1) {
+				update();
+				repaint();
+				delta--;
+			}
+		}
 		
 	}
 	
