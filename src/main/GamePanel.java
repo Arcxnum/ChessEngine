9621635e0ba919ddcbb33d45d20070b943ad2075
Arcxnum <gamingfardsletsgo@gmail.com>
2024-12-10 16:32:40 -0500

    Trying to get git pull to work

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,10 +1,21 @@
 package main;
 
-import javax.swing.JPanel;
-import java.awt.*;
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+import piece.Bishop;
+import piece.King;
+import piece.Knight;
+import piece.Pawn;
 import piece.Piece;
-import piece.*;
+import piece.Queen;
+import piece.Rook;
 public class GamePanel extends JPanel implements Runnable {
 
 	public static final int WIDTH = 1100;
@@ -12,34 +23,34 @@ public class GamePanel extends JPanel implements Runnable {
 	final int FPS = 60;
 	Thread gameThread;
 	Board board = new Board();
-	
-	
+
+
 	// Pieces
-	public static ArrayList<Piece> pieces = new ArrayList<Piece>();
-	public static ArrayList<Piece> simPieces = new ArrayList<Piece>();
+	public static ArrayList<Piece> pieces = new ArrayList<>();
+	public static ArrayList<Piece> simPieces = new ArrayList<>();
 	Piece activeP;
-	
+
 	// Color
 	public static final int WHITE = 0;
 	public static final int BLACK = 1;
 	int currentColor = WHITE;
-	
-	public GamePanel () {	
-		
+
+	public GamePanel () {
+
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		setBackground(Color.black);
-		
-		
+
+
 		setPieces();
 		copyPieces(pieces, simPieces);
-	
+
 	}
-	
+
 	public void launchGame() {
 		gameThread = new Thread(this);
-		gameThread.start();	
+		gameThread.start();
 	}
-	
+
 	public void setPieces() {
 		// White pieces
 		pieces.add(new Pawn(WHITE, 0, 6));
@@ -50,19 +61,19 @@ public class GamePanel extends JPanel implements Runnable {
 		pieces.add(new Pawn(WHITE, 5, 6));
 		pieces.add(new Pawn(WHITE, 6, 6));
 		pieces.add(new Pawn(WHITE, 7, 6));
-		
+
 		pieces.add(new Knight(WHITE, 1, 7));
 		pieces.add(new Knight(WHITE, 6, 7));
-		
+
 		pieces.add(new Rook(WHITE, 0, 7));
 		pieces.add(new Rook(WHITE, 7, 7));
-		
+
 		pieces.add(new Bishop(WHITE, 2, 7));
 		pieces.add(new Bishop(WHITE, 5, 7));
-		
+
 		pieces.add(new King(WHITE, 4, 7));
 		pieces.add(new Queen(WHITE, 3, 7));
-		
+
 		// Black pieces
 		pieces.add(new Pawn(BLACK, 0, 1));
 		pieces.add(new Pawn(BLACK, 1, 1));
@@ -72,42 +83,42 @@ public class GamePanel extends JPanel implements Runnable {
 		pieces.add(new Pawn(BLACK, 5, 1));
 		pieces.add(new Pawn(BLACK, 6, 1));
 		pieces.add(new Pawn(BLACK, 7, 1));
-		
+
 		pieces.add(new Knight(BLACK, 1, 0));
 		pieces.add(new Knight(BLACK, 6, 0));
-		
+
 		pieces.add(new Rook(BLACK, 0, 0));
 		pieces.add(new Rook(BLACK, 7, 0));
-		
+
 		pieces.add(new Bishop(BLACK, 2, 0));
 		pieces.add(new Bishop(BLACK, 5, 0));
-		
+
 		pieces.add(new King(BLACK, 4, 0));
 		pieces.add(new Queen(BLACK, 3, 0));
 	}
-	
+
 	private void copyPieces(ArrayList<Piece> source, ArrayList<Piece> target) {
 		target.clear();
 		for (int i = 0; i < source.size(); i++) {
 			target.add(source.get(i));
 		}
 	}
-	
+
 	@Override
 	public void run() {
-		
+
 		// GAME LOOP
 		double drawInterval = 1000000000/FPS;
 		double delta = 0;
 		long lastTime = System.nanoTime();
 		long currentTime;
-		
+
 		while (gameThread != null) {
 			currentTime = System.nanoTime();
-			
+
 			delta += (currentTime - lastTime)/drawInterval;
 			lastTime = currentTime;
-			
+
 			if (delta >= 1) {
 				update();
 				repaint();
@@ -116,10 +127,10 @@ public class GamePanel extends JPanel implements Runnable {
 		}
 
 	}
-	
+
 	private void update() {
 		// Mouse button pressed
-		
+
 			if (activeP == null) {
 				// If the activeP is null, check if you can pick up a piece
 				for (Piece piece : simPieces) {
@@ -128,49 +139,50 @@ public class GamePanel extends JPanel implements Runnable {
 					}
 				}
 			}
-		
+
 			// If the player is holding a piece, simulate the move
-			
-		
-	 
+
+
+
 		// Mouse button released
-			
-		
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 	private void simulate() {
 		// If a piece is being held, update its position
-		
+
 		activeP.col = activeP.getCol(activeP.x);
 		activeP.row = activeP.getRow(activeP.y);
 	}
-	
+
+	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		Graphics2D g2 = (Graphics2D)g;
-		
+
 		// Board
 		board.draw(g2);
-		
+
 		// Pieces
 		for (Piece p : simPieces) {
 			p.draw(g2);
 		}
-		
+
 		if (activeP != null) {
 			g2.setColor(Color.white);
 			g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7f));
 			g2.fillRect(activeP.col * Board.SQUARE_SIZE, activeP.row * Board.SQUARE_SIZE, Board.SQUARE_SIZE, Board.SQUARE_SIZE);
 			g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
-			
+
 			// Draw the active piece in the end so it won't be hidden by the board or the colored square
 			activeP.draw(g2);
 		}
 	}
-	
+
 }
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,21 +1,22 @@
 package main;
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		JFrame window = new JFrame("Chess");
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		window.setResizable(false);
-		
+
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);
 		window.pack();
-		
+
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
-		
+
 		gamePanel.launchGame();
 
 	}
