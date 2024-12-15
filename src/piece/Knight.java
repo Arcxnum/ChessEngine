c06c9996af0b1112029cb29725ba2e9a1b7776a6
Arcxnum <gamingfardsletsgo@gmail.com>
2024-12-15 18:43:47 -0500

    Added knight movement

--- src/piece/Knight.java
+++ src/piece/Knight.java
@@ -16,5 +16,17 @@ public class Knight extends Piece {
 		
 	}
 	
+	public boolean canMove(int targetCol, int targetRow) {
+		if (isWithinBoard(targetCol, targetRow)) {
+			// The knight's movement ratio (horizontal:vertical) is always 2:1 or 1:2.
+			if (Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 2) {
+				if (isValidSquare(targetCol, targetRow)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 }
 
