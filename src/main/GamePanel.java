70890e2fb2518cfb144b96f269545b8ca2ef2e85
Arcxnum <gamingfardsletsgo@gmail.com>
2024-12-15 19:24:34 -0500

    Added queen movement

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -66,7 +66,7 @@ public class GamePanel extends JPanel implements Runnable {
 		pieces.add(new Bishop(WHITE, 5, 4));
 		
 		pieces.add(new King(WHITE, 4, 4));
-		pieces.add(new Queen(WHITE, 3, 7));
+		pieces.add(new Queen(WHITE, 3, 4));
 		
 		// Black pieces
 		pieces.add(new Pawn(BLACK, 0, 1));
